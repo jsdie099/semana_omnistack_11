c8492505711acb0b264a7d414cc7320840854d7b
Juliano <julianopaulo.santos@hotmail.com>
2020-03-24 16:15:30 -0300

    Back-end da nossa aplicação

---
+++ backend/src/controllers/ongController.js
@@ -0,0 +1,25 @@
+const crypto = require('crypto');
+const connection = require("../database/connection");
+module.exports = {
+    async index(request,response){
+        const ongs = await connection('ongs').select('*');
+        return response.json(ongs);
+    },
+    async create(request,response){
+        //const params = request.query; //ex: /users= http://localhost:3333/users/name=juliano&idade=21
+        //const params = request.params; //ex: /users/:id = http://localhost:3333/users/1
+        //const params = request.body;
+        //console.log(params)
+        const {name, email, whatsapp, city, uf} = request.body;
+        const id = crypto.randomBytes(4).toString('HEX');
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        })
+        return response.json({ id });
+    }
+};
